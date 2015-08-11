86f7b8e9eb3d98b0e48e522098bc3e5e367b710c
Pei Pei <Pei.Pei@D2L.com>
2015-08-11 10:48:37 -0400

    can load the master and 3 intro views

--- WalkthroughDemo/WalkthroughDemo/ViewController.swift
+++ WalkthroughDemo/WalkthroughDemo/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BWWalkthroughViewControllerDelegate {
 
+    @IBAction func showWalkthBuPressed(sender: AnyObject) {
+        
+        print("showWalkthBuPressed")
+        
+        // Get view controllers and build the walkthrough
+        let stb = UIStoryboard(name: "Main", bundle: nil)
+        let walkthrough = stb.instantiateViewControllerWithIdentifier("walk0") as! BWWalkthroughViewController
+        let page_one = stb.instantiateViewControllerWithIdentifier("walk1") as! UIViewController
+        let page_two = stb.instantiateViewControllerWithIdentifier("walk2")as! UIViewController
+        let page_three = stb.instantiateViewControllerWithIdentifier("walk3")as! UIViewController
+        
+        // Attach the pages to the master
+        walkthrough.delegate = self
+        walkthrough.addViewController(page_one)
+        walkthrough.addViewController(page_two)
+        walkthrough.addViewController(page_three)
+        
+        self.presentViewController(walkthrough, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
